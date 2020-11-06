b84056c5dddfae68e5988770720e4899f9da5461
tuan <adgtuan@gmail.com>
2020-11-06 22:46:41 +0700

    Updates

---
+++ precache-manifest.d460ce488e793fd5a27f73c5fa849e32.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17f3565682aaee715e80e876c84ea31d",
+    "url": "/myfunretro/index.html"
+  },
+  {
+    "revision": "72d3cee91921a93a4950",
+    "url": "/myfunretro/static/css/main.0a682071.chunk.css"
+  },
+  {
+    "revision": "5ce359492e917d036225",
+    "url": "/myfunretro/static/js/2.e65ae7cc.chunk.js"
+  },
+  {
+    "revision": "df23304fbfc64bbf73a0f148aa1912f9",
+    "url": "/myfunretro/static/js/2.e65ae7cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72d3cee91921a93a4950",
+    "url": "/myfunretro/static/js/main.9aed0889.chunk.js"
+  },
+  {
+    "revision": "ae68578992e069f7fd43",
+    "url": "/myfunretro/static/js/runtime-main.f2790161.js"
+  }
+]);
